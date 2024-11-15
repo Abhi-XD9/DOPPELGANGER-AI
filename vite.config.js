008283cf9774cb132780c1f8c097ae80b0de336e
Thirmalreddy002 <thirumalreddy002@gmail.com>
2024-11-15 15:47:07 +0530

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base:"/FaceSearch-AI/",
   build: {
     rollupOptions: {
       input: '/src/main.jsx', 
