f39b27a8ec6b9bd4b7eaa8623cb8089346a328a5
Abhishek Anugu <158754433+Abhi-XD9@users.noreply.github.com>
2024-11-15 20:28:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base:"/FaceSearch-AI/",
+  base:"/DOPPELGANGER-AI/",
   build: {
     rollupOptions: {
       input: '/src/main.jsx', 
